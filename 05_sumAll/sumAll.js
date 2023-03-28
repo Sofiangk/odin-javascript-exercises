5e2309e147cac2a660e4b690f136c31ae7c8b887
sofiangk <sofian.a.r.gargouri@gmail.com>
2023-03-28 13:46:15 +0200

    Complete sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function (startNum, endNum) {
+  let sum = 0;
+  if (
+    !Number.isInteger(startNum) ||
+    !Number.isInteger(endNum) ||
+    endNum < 0 ||
+    startNum < 0
+  ) {
+    return "ERROR";
+  }
+  if (startNum > endNum) {
+    const temp = startNum;
+    startNum = endNum;
+    endNum = temp;
+  }
+  for (let i = startNum; i <= endNum; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
