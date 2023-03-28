a063bdb1072d33c6c7720c57aefa656858499a5c
sofiangk <sofian.a.r.gargouri@gmail.com>
2023-03-28 16:02:08 +0200

    Complete palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+  let noSpacesString = text
+    .replace(/\s+/g, "")
+    .toLowerCase()
+    .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+  let reversedString = noSpacesString
+    .split("")
+    .reverse()
+    .join("")
+    .toLowerCase();
+  return noSpacesString === reversedString;
 };
 
 // Do not edit below this line
