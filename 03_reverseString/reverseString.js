e1781d5de47d463ff8acdb74acf04b61a05cd42d
sofiangk <sofian.a.r.gargouri@gmail.com>
2023-03-28 12:30:47 +0200

    Complete reverseString.js exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function (text) {
+  let string = "";
+  for (let i = text.length - 1; i >= 0; i--) {
+    string += text[i];
+  }
 
+  return string;
 };
+
+reverseString("hello there");
 
 // Do not edit below this line
 module.exports = reverseString;
